f67343482f5a9f36f0ba4f18e24135c3343e2d24
peter zhang <peterzhang@peters-iMac.attlocal.net>
2022-02-01 23:05:41 -0800

    day 6

--- input.py
+++ input.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-from sqlite3 import Date
-from xmlrpc.client import DateTime
-
 
 journal = input("please enter your journal : ")
 date = datetime.today().date()
 print("your journal is : ", journal + "\nyour journal date is  : ", date )
 
+
+journal_date = input('what date is it : ')
+print(journal_date)
+
+journal_text = input('Enter your text below \n')
+print(journal_text)
 
