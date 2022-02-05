d9472fed12617b4e591cee1e1bddfd8544720421
peter zhang <peterzhang@peters-iMac.attlocal.net>
2022-02-05 00:58:17 -0800

    7 day

--- input.py
+++ input.py
@@ -1,13 +1,34 @@
 from datetime import datetime
 
-journal = input("please enter your journal : ")
-date = datetime.today().date()
-print("your journal is : ", journal + "\nyour journal date is  : ", date )
+# journal = input("please enter your journal : ")
+# date = datetime.today().date()
+# print("your journal is : ", journal + "\nyour journal date is  : ", date )
 
 
-journal_date = input('what date is it : ')
-print(journal_date)
+# journal_date = input('what date is it : ')
+# print(journal_date)
 
-journal_text = input('Enter your text below \n')
-print(journal_text)
+# journal_text = input('Enter your text below \n')
+# print(journal_text)
 
+
+# New Screen
+# print("\n")
+# print("Date: ",journal_date, "\n")
+# print("This is a sample entry yada \n")
+# print("\n")
+
+
+print("")
+journal_date = input("Date: ")
+journal_text = input("\n")
+print("")
+
+print("Bouns Fun")
+print("")
+print(f"Date: "+ journal_date + "\n")
+
+print(journal_text + "\n")
+print("")
+
+print(f"Bouns Fun \n\nDate: {journal_date}\n\n{journal_text}\n")
